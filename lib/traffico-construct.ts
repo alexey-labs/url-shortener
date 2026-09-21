28a9dbe285031e9dc4f2411b1976716f16444a0c
Alexey Sergeev <xengawe@gmail.com>
2026-09-21 16:30:45 -0700

    add logs and fix bootloop

--- lib/traffico-construct.ts
+++ lib/traffico-construct.ts
@@ -1,7 +1,9 @@
 import { Construct } from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as logs from "aws-cdk-lib/aws-logs";
 import * as path from "path";
+import * as ecrAssets from "aws-cdk-lib/aws-ecr-assets";
 
 interface TrafficoProps {
   vpc: ec2.IVpc;
@@ -14,13 +16,23 @@ export class Traffico extends Construct {
     super(scope, `${id}-construct`);
 
     const cluster = new ecs.Cluster(this, "Cluster", { vpc: props.vpc });
+    const logGroup = new logs.LogGroup(this, "PingerLogs", {
+      retention: logs.RetentionDays.ONE_DAY,
+    });
 
     const taskDef = new ecs.FargateTaskDefinition(this, "PingerTask");
+
     taskDef.addContainer("Pinger", {
-      image: ecs.ContainerImage.fromAsset(path.join(__dirname, "./pinger")),
+      image: ecs.ContainerImage.fromAsset(path.join(__dirname, "../pinger"), {
+        platform: ecrAssets.Platform.LINUX_AMD64,
+      }),
       environment: {
         URL: props.url,
       },
+      logging: ecs.LogDrivers.awsLogs({
+        logGroup,
+        streamPrefix: "pinger",
+      }),
     });
 
     new ecs.FargateService(this, "PingerService", {
